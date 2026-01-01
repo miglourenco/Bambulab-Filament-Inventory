99a57d17906c8b63bdbc61eaefd5d8496f023d57
miglourenco <miguel123369@gmail.com>
2026-01-01 03:52:45 +0000

    Update RegisterView.vue

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -1,107 +1,103 @@
 <template>
-  <v-container class="fill-height">
-    <v-row align="center" justify="center">
-      <v-col cols="12" sm="8" md="6" lg="4">
-        <v-card elevation="8" rounded="lg">
-          <v-card-title class="text-h5 text-center pa-6 bg-primary">
-            <v-icon size="large" class="mr-2" color="white">mdi-account-plus</v-icon>
-            <span class="text-white">Create Account</span>
-          </v-card-title>
+  <div class="d-flex align-center justify-center" style="height: 100vh">
+    <v-card width="400" elevation="8" rounded="lg">
+      <v-card-title class="text-h5 text-center pa-6 bg-primary">
+        <v-icon size="large" class="mr-2" color="white">mdi-account-plus</v-icon>
+        <span class="text-white">Create Account</span>
+      </v-card-title>
 
-          <v-card-text class="pa-6">
-            <v-form v-model="valid" @submit.prevent="registerUser">
-              <v-text-field
-                v-model="username"
-                label="Username"
-                :rules="requiredRules"
-                prepend-inner-icon="mdi-account"
-                variant="outlined"
-                class="mb-2"
-              ></v-text-field>
+      <v-card-text class="pa-6">
+        <v-form v-model="valid" @submit.prevent="registerUser">
+          <v-text-field
+            v-model="username"
+            label="Username"
+            :rules="requiredRules"
+            prepend-inner-icon="mdi-account"
+            variant="outlined"
+            class="mb-2"
+          ></v-text-field>
 
-              <v-text-field
-                v-model="email"
-                label="Email"
-                :rules="emailRules"
-                prepend-inner-icon="mdi-email"
-                variant="outlined"
-                type="email"
-                class="mb-2"
-              ></v-text-field>
+          <v-text-field
+            v-model="email"
+            label="Email"
+            :rules="emailRules"
+            prepend-inner-icon="mdi-email"
+            variant="outlined"
+            type="email"
+            class="mb-2"
+          ></v-text-field>
 
-              <v-text-field
-                v-model="adminKey"
-                label="Admin Registration Key"
-                :rules="requiredRules"
-                prepend-inner-icon="mdi-key-variant"
-                :type="showAdminKey ? 'text' : 'password'"
-                :append-inner-icon="showAdminKey ? 'mdi-eye' : 'mdi-eye-off'"
-                @click:append-inner="showAdminKey = !showAdminKey"
-                variant="outlined"
-                hint="Contact administrator for registration key"
-                persistent-hint
-                class="mb-2"
-              ></v-text-field>
+          <v-text-field
+            v-model="adminKey"
+            label="Admin Registration Key"
+            :rules="requiredRules"
+            prepend-inner-icon="mdi-key-variant"
+            :type="showAdminKey ? 'text' : 'password'"
+            :append-inner-icon="showAdminKey ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append-inner="showAdminKey = !showAdminKey"
+            variant="outlined"
+            hint="Contact administrator for registration key"
+            persistent-hint
+            class="mb-2"
+          ></v-text-field>
 
-              <v-text-field
-                v-model="password"
-                label="Password"
-                :rules="passwordRules"
-                prepend-inner-icon="mdi-lock"
-                :type="showPassword ? 'text' : 'password'"
-                :append-inner-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                @click:append-inner="showPassword = !showPassword"
-                variant="outlined"
-                class="mb-2"
-              ></v-text-field>
+          <v-text-field
+            v-model="password"
+            label="Password"
+            :rules="passwordRules"
+            prepend-inner-icon="mdi-lock"
+            :type="showPassword ? 'text' : 'password'"
+            :append-inner-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append-inner="showPassword = !showPassword"
+            variant="outlined"
+            class="mb-2"
+          ></v-text-field>
 
-              <v-text-field
-                v-model="confirmPassword"
-                label="Confirm Password"
-                :rules="confirmPasswordRules"
-                prepend-inner-icon="mdi-lock-check"
-                :type="showConfirmPassword ? 'text' : 'password'"
-                :append-inner-icon="showConfirmPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                @click:append-inner="showConfirmPassword = !showConfirmPassword"
-                variant="outlined"
-                class="mb-4"
-              ></v-text-field>
+          <v-text-field
+            v-model="confirmPassword"
+            label="Confirm Password"
+            :rules="confirmPasswordRules"
+            prepend-inner-icon="mdi-lock-check"
+            :type="showConfirmPassword ? 'text' : 'password'"
+            :append-inner-icon="showConfirmPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append-inner="showConfirmPassword = !showConfirmPassword"
+            variant="outlined"
+            class="mb-4"
+          ></v-text-field>
 
-              <v-alert v-if="error" type="error" variant="tonal" class="mb-4">
-                {{ error }}
-              </v-alert>
+          <v-alert v-if="error" type="error" variant="tonal" class="mb-4">
+            {{ error }}
+          </v-alert>
 
-              <v-btn
-                type="submit"
-                color="primary"
-                size="large"
-                block
-                :disabled="!valid"
-                :loading="loading"
-              >
-                <v-icon start>mdi-account-plus</v-icon>
-                Register
-              </v-btn>
+          <v-btn
+            type="submit"
+            color="primary"
+            size="large"
+            block
+            :disabled="!valid"
+            :loading="loading"
+          >
+            <v-icon start>mdi-account-plus</v-icon>
+            Register
+          </v-btn>
 
-              <v-divider class="my-4"></v-divider>
+          <v-divider class="my-4"></v-divider>
 
-              <div class="text-center">
-                <span class="text-body-2">Already have an account?</span>
-                <v-btn
-                  variant="text"
-                  color="primary"
-                  @click="router.push({ name: 'Login' })"
-                  class="ml-1"
-                >
-                  Login
-                </v-btn>
-              </div>
-            </v-form>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+          <div class="text-center">
+            <span class="text-body-2">Already have an account?</span>
+            <v-btn
+              variant="text"
+              color="primary"
+              @click="router.push({ name: 'Login' })"
+              class="ml-1"
+            >
+              Login
+            </v-btn>
+          </div>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script setup>
@@ -174,11 +170,3 @@ const registerUser = async () => {
   }
 };
 </script>
-
-<style scoped>
-.fill-height {
-  min-height: 100vh;
-  display: flex;
-  align-items: center;
-}
-</style>
